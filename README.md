a7940b3662043d7618805a9d5c87831f3dbead70
Tony Majestro <tonymajestro@gmail.com>
2024-01-13 15:01:03 -0800

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # s3sh
+## Description
+An iteractive command line shell for navigating and manipulating files in AWS S3.
+
+## Usage
+By default, s3sh will use the credentials in your ~/.aws/ directory.
+
+
+```
+npx s3sh
+
+/$ cd my-bucket-name
+/$ ls
+directory1/
+directory2/
+file.txt
+
+/$ cd directory1
+/directory1/$ ls
+directory3/
+file2.txt
+file3.txt
+```
+
+## Tab completion
+s3sh supports tab completion for available commands. It also supports autocompleting directory and folder names.
--- src/autocomplete.js
+++ src/autocomplete.js
@@ -7,18 +7,18 @@ const commands = [
 
 const autocomplete = (shell) => {
   const wrapper = (line, callback) => {
-    const parts = line.split(/\s+/);
+    const parts = line.trimLeft().split(/\s+/);
 
     if (parts.length <= 1) {
       // autocomplete the available commands
-      const hits = commands.filter((c) => c.startsWith(line));
-      callback(null, [hits.length ? hits : commands, line]);
+      const hits = commands.filter((c) => c.startsWith(parts[0]));
+      callback(null, [hits.length ? hits : commands, parts[0]]);
     } else if (parts.length == 2 && parts[0].trim() === 'cd') {
       // autocomplete buckets or s3 objects
       shell.ls().then(paths => {
         const argument = parts[1];
         const hits = paths.filter((c) => c.startsWith(argument));
-        callback(null, [hits.length ? hits : lsObjects, argument]);
+        callback(null, [hits.length ? hits : paths, argument]);
       });
     } else {
       callback(null, [[], line]);
